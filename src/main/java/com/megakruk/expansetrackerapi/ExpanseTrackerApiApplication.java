cefa04fe708d35ea8de72c060af79632b6148334
MegaKruk <filip.mazur.dev@gmail.com>
2021-09-03 23:54:57 +0200

    Implemented simple JWT authentication

--- src/main/java/com/megakruk/expansetrackerapi/ExpanseTrackerApiApplication.java
+++ src/main/java/com/megakruk/expansetrackerapi/ExpanseTrackerApiApplication.java
@@ -1,13 +1,24 @@
 package com.megakruk.expansetrackerapi;
 
+import com.megakruk.expansetrackerapi.filters.AuthFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ExpanseTrackerApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ExpanseTrackerApiApplication.class, args);
+	public static void main(String[] args) { SpringApplication.run(ExpanseTrackerApiApplication.class, args); }
+
+	@Bean
+	public FilterRegistrationBean<AuthFilter> filterFilterRegistrationBean() {
+		FilterRegistrationBean<AuthFilter> registrationBean =
+				new FilterRegistrationBean<>();
+		AuthFilter authFilter = new AuthFilter();
+		registrationBean.setFilter(authFilter);
+		registrationBean.addUrlPatterns("/api/categories/*");
+		return registrationBean;
 	}
 
 }
